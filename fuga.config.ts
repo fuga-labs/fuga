59184b9a128a108eabc83240f9031e22dee2fc0d
Aman Deep <deep.aman1729@gmail.com>
2024-11-06 11:39:13 +0530

    Updated fuga.config.ts

--- fuga.config.ts
+++ fuga.config.ts
@@ -1,16 +1,9 @@
 export const fugaConfig = {
 	theme: {},
 	extends: {
-		colors: {
-			green: '#473389',
-			red: '#949490',
-			grey: '#dkdks',
-			blue: '#dkdksu'
-		},
+		colors: {},
 		fontFamily: {},
-		sizes: {
-			large: '180px'
-		}
+		sizes: {}
 	},
 	components: {}
 };
